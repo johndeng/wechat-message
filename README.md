583c08a2448f5bc523545a05c1c337708cd0e5ee
John <denghuanzhong@gmail.com>
2014-10-07 23:37:56 +0800

    init repository

---
+++ README.md
@@ -0,0 +1,3 @@
+WeChat Message Utils
+---
+
---
+++ wemsg/parser.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+
+class WechatParser(object):
+    """ Message Parser
+    """
+    def __init__(self):
+        pass
+
+    def __get__(self):
+        pass
